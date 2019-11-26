ae6dfc2b2ab026140e8bdcfad5fd8d89af8e428a
sbx_user1051 <sbx_user1051@169.254.44.181>
2019-11-26 10:39:41 +0000

    Tue Nov 26 10:39:41 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -27,15 +27,35 @@ public class FizzBuzzSolution {
     }
     return check;
   }
+  public boolean checkDeluxe(Integer number) {
+    if (number <= 10) {
+      return false;
+    }
+    Integer copy = number;
+    int lastDigit = copy % 10;
+    copy = copy/10;
+    while(copy > 0) {
+      int c = copy % 10;
+      if(c != lastDigit) {
+        return false;
+      }
+      copy = copy/10;
+    }
+    return true;
+  }
 
     public String fizzBuzz(Integer number) {
+
        if((number % 3 == 0 || checkHasThree(number) == true )
-           &&  (number % 5 == 0 || checkHasFive(number) == true) ) {
-         return "fizz buzz";
+           &&  (number % 5 == 0 || checkHasFive(number) == true) && checkDeluxe(number) == true) {
+           return "fizz buzz deluxe";
+       } else if((number % 3 == 0 || checkHasThree(number) == true )
+             &&  (number % 5 == 0 || checkHasFive(number) == true) ) {
+           return "fizz buzz";
        } else if (number % 3 == 0 || checkHasThree(number) == true) {
-         return "fizz";
+           return "fizz";
        } else if(number % 5 == 0 || checkHasFive(number) == true) {
-         return "buzz";
+           return "buzz";
        }
 
        return number.toString();
@@ -43,5 +63,6 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
 
