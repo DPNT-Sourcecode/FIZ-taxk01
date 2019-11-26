53c4504c4e06d3de0869aa8d09025fd31c35b2cf
sbx_user1051 <sbx_user1051@169.254.44.181>
2019-11-26 10:34:00 +0000

    Tue Nov 26 10:34:00 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,38 +5,43 @@ public class FizzBuzzSolution {
   public boolean checkHasThree(Integer number) {
     boolean check = false;
     Integer copy = number;
-    while (copy) {
+    while (copy > 0) {
       int c = copy % 10;
       if (c == 3) {
         return true;
       }
       copy = copy / 10;
     }
+    return check;
   }
 
   public boolean checkHasFive(Integer number) {
     boolean check = false;
     Integer copy = number;
-    while (copy) {
+    while (copy > 0) {
       int c = copy % 10;
       if (c == 5) {
         return true;
       }
       copy = copy / 10;
     }
+    return check;
   }
 
     public String fizzBuzz(Integer number) {
-        if ((number % 3 == 0) && (number % 5 == 0)) {
-          return "fizz buzz";
-        } else if (number % 3 == 0) {
-          return "fizz";
-        } else if (number % 5 == 0) {
-          return "buzz";
-        } else {
-          return number.toString();
-        }
+       if((number % 3 == 0 || checkHasThree(number) == true )
+           &&  (number % 5 == 0 || checkHasFive(number) == true) ) {
+         return "fizz buzz";
+       } else if (number % 3 == 0 || checkHasThree(number) == true) {
+         return "fizz";
+       } else if(number % 5 == 0 || checkHasFive(number) == true) {
+         return "buzz";
+       }
+
+       return number.toString();
+
     }
 
 }
+
 
